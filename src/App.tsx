db3caae64da0c3bc1e2df4512139d383f497c93c
Luiz Antonio Rozendo <luiz.rozendo01@gmail.com>
2024-08-17 20:16:30 -0300

    add some pieces to the board

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { Board } from './components/Board/Board';
+import data from './example.json';
 import BoardModel from './models/BoardModel';
+import { Labels } from './models/Labels';
+
 function App() {
   const [board, setBoard] = useState<BoardModel>(new BoardModel())
+  const [gameState, setGameState] = useState<Boolean | {
+    piece_color: string;
+    piece_type: string;
+    piece_position: number[];
+  }>();
+
+
+  function showState(board: BoardModel) {
+    for (let row = 0; row < data.state.length; row++) {
+      for (let col = 0; col < data.state[row].length; col++) {
+        if (typeof (data.state[row][col]) === "object") {
+          board.addFigure(data.state[row][col].piece_color === "black" ? Labels.Dark : Labels.Light, data.state[row][col].piece_position[1], data.state[row][col].piece_position[0]);
+        }
+      }
+    }
+  }
 
   const restart = () => {
     const newBoard = new BoardModel();
     newBoard.createCells();
+    setGameState(data.state);
+    showState(newBoard);
     setBoard(newBoard);
   }
 
