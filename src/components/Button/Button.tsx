31afbaff8aecc889c5b1e51bce16f472d588d629
Luiz Antonio Rozendo <luiz.rozendo01@gmail.com>
2024-08-31 17:54:19 -0300

    add disable and className as parameter of button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,13 +5,23 @@ interface ButtonProps {
     onClick?: () => void | null
     onSubmit?: () => void | null
     type?: "button" | "submit" | "reset";
-    className?: string
+    className?: string,
+    disabled?: boolean
 }
 
-export const Button = ({ value, type, className, onClick, onSubmit }: ButtonProps): ReactElement => {
+export const Button = ({ value, type, className, onClick, onSubmit, disabled }: ButtonProps): ReactElement => {
+    const defaultClasses = "bg-blue-500 text-white font-bold py-2 px-4 rounded w-full mt-3 mb-3";
+    const enabledClasses = "hover:bg-blue-700";
+    const disabledClasses = "bg-gray-400 text-gray-700 cursor-not-allowed";
 
     return (
-        <button type={type} className={className ? className : "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full mt-3 mb-3"} onClick={onClick} onSubmit={onSubmit}>
+        <button
+            type={type}
+            className={`${className ? className : defaultClasses} ${disabled ? disabledClasses : enabledClasses}`}
+            onClick={onClick}
+            onSubmit={onSubmit}
+            disabled={disabled}
+        >
             {value}
         </button>
     )
