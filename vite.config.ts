42895f314cad961ffb2dab9c95af1bc81c42d2ca
Luiz Antonio Rozendo <luiz.rozendo01@gmail.com>
2025-01-03 22:53:43 -0300

    fix error https

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://201.23.16.78:8000', // Endpoint do seu backend
+        changeOrigin: true,
+        secure: false, // Não verifique o SSL (caso esteja usando HTTP localmente)
+      }
+    }
   },
   plugins: [react()],
 })
